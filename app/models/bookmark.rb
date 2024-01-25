d059fa41d552ec64048320a1a0403c6d144e0308
Julia Pfaffenzeller <julia.pfaffenzeller@web.de>
2024-01-25 12:58:38 +0100

    tried

---
+++ app/models/bookmark.rb
@@ -0,0 +1,6 @@
+class Bookmark < ApplicationRecord
+  belongs_to :list
+  belongs_to :movie
+  validates :list, :movie, presence: true
+  validates :comment, length: { minimum: 6 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_25_104442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "list", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.text "overview"
+    t.string "poster_url"
+    t.float "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
