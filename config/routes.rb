4cd2c8cdb9e5dc74d0c1bf714c694413eeb21d49
Julia Pfaffenzeller <julia.pfaffenzeller@web.de>
2024-01-25 15:39:50 +0100

    fomr for bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,20 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  # A user can see all the lists
-  get "lists", to: "lists#index"
+  # # A user can see all the lists
+  # get "lists", to: "lists#index"
 
-    # A user can create a new list
-    get "lists/new", to: "lists#new"
-    post "lists", to: "lists#create"
+  #   # A user can create a new list
+  #   get "lists/new", to: "lists#new"
+  #   post "lists", to: "lists#create"
 
-  # A user can see the details of a given list and its name
-  get "lists/:id", to: "lists#show", as: "list"
+  # # A user can see the details of a given list and its name
+  # get "lists/:id", to: "lists#show", as: "list"
 
-
+  # # A user can add a new bookmark (movie/list pair) to an existing list
+  Rails.application.routes.draw do
+    resources :lists, only: [:index, :new, :create, :show] do
+      resources :bookmarks, only: [:new, :create]
+    end
+  end
 end
