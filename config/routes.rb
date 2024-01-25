9e7b4a5bbf1b090adbddfe2809d49115a2fdd41c
Julia Pfaffenzeller <julia.pfaffenzeller@web.de>
2024-01-25 15:16:25 +0100

    list completed

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # A user can see all the lists
+  get "lists", to: "lists#index"
+
+    # A user can create a new list
+    get "lists/new", to: "lists#new"
+    post "lists", to: "lists#create"
+
+  # A user can see the details of a given list and its name
+  get "lists/:id", to: "lists#show", as: "list"
+
+
 end
